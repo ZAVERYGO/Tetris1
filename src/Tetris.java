5f99b40407ea44ae0b70198be667ce45bac40538
ZAVERYGO <Hockey.nick932@gmail.com>
2023-09-05 21:08:42 +0300

    2

--- src/Tetris.java
+++ src/Tetris.java
@@ -7,11 +7,6 @@ public class Tetris {
         windowUser.add(new Text());
 
         windowUser.addFigure();
-        /*JFrame jFrame = new JFrame();
-        jFrame.setSize(300, 300);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
-        jFrame.add(new Paint());*/
+
     }
 }
