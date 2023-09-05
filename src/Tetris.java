dd343d10f4079194ea70c690075ded2ece38c141
ZAVERYGO <Hockey.nick932@gmail.com>
2023-09-05 21:17:17 +0300

    2

--- src/Tetris.java
+++ src/Tetris.java
@@ -4,8 +4,6 @@ import interfaceUser.WindowUser;
 public class Tetris {
     public static void main(String[] args) {
         WindowUser windowUser = new WindowUser();
-        windowUser.add(new Text());
-
         windowUser.addFigure();
 
     }
